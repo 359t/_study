533c7a9efb3a73fe4c299bab7fe7094881e4b039
liuhao-home <1550144845@qq.com>
2025-12-28 00:22:15 +0800

    project build

---
+++ 01英语演讲比赛/include/speechManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+class speechManager
+{
+    public:
+
+    speechManager();
+
+    ~speechManager();
+
+    void show_Menu();
+
+};
---
+++ 01英语演讲比赛/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "speechManager.h"
+using namespace std;
+
+int main()
+{
+    speechManager sm;
+
+    sm.show_Menu();
+
+    return 0;
+}
